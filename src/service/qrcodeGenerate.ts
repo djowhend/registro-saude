8624e911eab14a4819c8ccbd2a4bbf8764361ecb
Dionatan <djowsss@gmail.com>
2024-01-31 13:22:53 -0300

    qrcodeGenerate

--- src/service/qrcodeGenerate.ts
+++ src/service/qrcodeGenerate.ts
@@ -3,7 +3,7 @@ import QRCode from "qrcode"
 // With promises
 QRCode.toDataURL('I am a pony!')
   .then(url => {
-    console.log(url)
+    console.log("teste "+url)
   })
   .catch(err => {
     console.error(err)
