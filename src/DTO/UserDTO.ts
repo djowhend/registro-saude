666e3ebeac878209f1276314575e5a588e202713
Dionatan <djowsss@gmail.com>
2024-02-01 21:34:40 -0300

    segundo commit

---
+++ src/DTO/UserDTO.ts
@@ -0,0 +1,36 @@
+import { User } from "../model/User";
+
+export class UserDTO {
+
+    constructor(
+        public id: number,
+        public nome: string,
+        public email: string,
+        public senha: string,
+        public cpf: string,
+    ) { }
+
+
+    static fromModel(user: User): UserDTO {
+        const userDTO = new UserDTO(
+        user.id,
+        user.nome,
+        user.email,
+        user.senha,
+        user.cpf
+        );
+        
+        return userDTO;
+    }
+
+    toModel(): User {
+        let newUser = new User();
+        newUser.nome = this.nome;
+        newUser.email = this.email;
+        newUser.senha = this.senha;
+        newUser.cpf = this.cpf;
+        return newUser;
+
+    }
+
+}
